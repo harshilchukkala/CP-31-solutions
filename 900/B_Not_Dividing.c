ae59281795f10492539091330d59254826d3d4eb
harshilchukkala <harshilchukkala@gmail.com>
2026-03-24 18:22:20 +0530

    solution

--- 900/B_Not_Dividing.c
+++ 900/B_Not_Dividing.c
@@ -12,11 +12,20 @@ int main(void)
         for(int i=0;i<n;i++)
         {
             scanf("%d", &arr[i]);
+            if(arr[i]==1) arr[i]++;
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i+1]%arr[i]==0)
+            {
+                arr[i+1]++;
+            }
         }
         for(int i=0;i<n;i++)
         {
-            arr[i]=0;
+            printf("%d ",arr[i]);
         }
+        printf("\n");
     }
     return 0;
 }
