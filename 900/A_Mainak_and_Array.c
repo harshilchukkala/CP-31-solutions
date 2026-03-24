5dab3db181f2e3cf624b11fb4ca37ab83e6ec22d
harshilchukkala <harshilchukkala@gmail.com>
2026-03-24 19:38:50 +0530

    solution

--- 900/A_Mainak_and_Array.c
+++ 900/A_Mainak_and_Array.c
@@ -9,20 +9,12 @@ int main(void)
         int n;
         scanf("%d", &n);
         int arr[n];
-        int max=0,min=1000,maxindex=0,minindex=0;
+        int max=0,min=1000;
         for(int i=0;i<n;i++)
         {
             scanf("%d", &arr[i]);
-            if(arr[i]<min)
-            {
-                min=arr[i];
-                maxindex=i;
-            }
-            if(arr[i]>max)
-            {
-                max=arr[i];
-                minindex=i;
-            }
+            if(arr[i]<min) min=arr[i];
+            if(arr[i]>max)  max=arr[i];
         }
         if(n==1)
         {
@@ -30,9 +22,12 @@ int main(void)
             continue;
         }
         int ans=arr[n-1]-arr[0];
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]-arr[i+1]>ans) ans=arr[i]-arr[i+1];
+        }
         if(max-arr[0]>ans) ans=max-arr[0];
         if(arr[n-1]-min>ans) ans=arr[n-1]-min;
-        if(maxindex-minindex==1 || maxindex-minindex==-1) ans=max-min;
         printf("%d\n",ans);
     }
     return 0;
