b5aee2117c0f3c7b5a7b48bd22467341fd429752
harshilchukkala <harshilchukkala@gmail.com>
2026-03-30 16:52:42 +0530

    solution

--- 900/B_Make_it_Divisible_by_25.c
+++ 900/B_Make_it_Divisible_by_25.c
@@ -39,13 +39,13 @@ int main(void)
         int min=50;
         int count1=count(0,0,len,digits);
         if(count1<min) min=count1;
-        int count2=count(2,5,len,digits);
-        if(count2<min) min=count2;
-        int count3=count(5,0,len,digits);
-        if(count3<min) min=count3;
-        int count4=count(7,5,len,digits);
-        if(count4<min) min=count4;
-        printf("%d\n",min);
+        // int count2=count(2,5,len,digits);
+        // if(count2<min) min=count2;
+        // int count3=count(5,0,len,digits);
+        // if(count3<min) min=count3;
+        // int count4=count(7,5,len,digits);
+        // if(count4<min) min=count4;
+        // printf("%d\n",min);
     }
     return 0;
 }
